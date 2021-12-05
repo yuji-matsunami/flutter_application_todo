617fe80bf69fb8bd2d499ec5337243d4f9bf7fee
yuji-matsunami <yuji.sohuto@gmail.com>
2021-12-06 01:37:12 +0900

    fix code

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 // グローバル変数で高さと横を保持
-double field_w=0.0;
-double field_h=0.0;
+double fieldW=0.0;
+double fieldH=0.0;
 
 void main() {
-  debugPaintSizeEnabled = true;
+  debugPaintSizeEnabled = false;
   runApp(MaterialApp(
     title: 'Flutter Todo',
     theme: ThemeData(
@@ -26,67 +26,17 @@ class MyApp extends StatelessWidget {
     // 画面サイズの計測を行う
     // 画面の高さはpaddingとAppBarの高さを引く
     Size size = MediaQuery.of(context).size;
-    field_w = size.width;
+    fieldW = size.width;
     var screenHeight = size.height;
     var abovePadding = MediaQuery.of(context).padding.top;
     var appBar = AppBar();
     var appBarHeight = appBar.preferredSize.height;
-    field_h = screenHeight - abovePadding - appBarHeight;
+    fieldH = screenHeight - abovePadding - appBarHeight;
 
     // var list = ["todo!","todo!","todo!","todo!","todo!","todo!"];
     return TodoMain();
-
-    /*
-    return MaterialApp(
-      title: 'Flutter Todo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: TodoMain(),
-      /*body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            if (index >= list.length) {
-              list.addAll(["todo_add", "todo_add"]);
-            }
-            return _menuItem(list[index]);
-          }
-        ),*/
-    );
-    */
+    
   }
-  /*
-  Widget _menuItem(String title) {
-    return Container(
-      decoration: new BoxDecoration(
-        border: new Border(bottom: BorderSide(width:1.0, color: Colors.grey))
-      ),
-      child: ListTile(
-        title:  Text(
-          title,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18.0
-          ),
-        ),
-        onTap: (){
-          print("ontap called.");
-        },
-        onLongPress: (){
-          print("on long tap called");
-        },
-      )
-    );
-  }
-  */
 }
 
 class TodoMain extends StatefulWidget {
@@ -99,13 +49,7 @@ class _TodoMainState extends State<TodoMain> {
   var flagList = [];
   var todoTitle;
   final TextEditingController _textEditingController = new TextEditingController();
-  /*
-  void _addTodo() {
-    setState(() {
-      list.add("todo!!!");
-    });
-  }
-  */
+  
   void _addTodoTitle(String title) {
     setState(() {
       list.add(title);
@@ -152,15 +96,11 @@ class _TodoMainState extends State<TodoMain> {
       child: Column(
         children: [
           Container(
-            height: (list.length <= 9) ? field_h/10 * list.length + 1 : field_h/10*9,
+            height: (list.length <= 9) ? fieldH/10 * list.length + 1 : fieldH/10*9,
             child: titlelist(),
           ),
           addtext(),
-          /*
-            FloatingActionButton(
-              onPressed: _addTodo,
-              child: Icon(Icons.add),
-            ),*/
+          
         ],
       ),
     );
@@ -176,7 +116,7 @@ class _TodoMainState extends State<TodoMain> {
 
   Widget showlist(String title, int idx) {
     return Container(
-      height: field_h/10,
+      height: fieldH/10,
       decoration: new BoxDecoration(
           border: new Border(bottom: BorderSide(color: Colors.grey, width: 3))),
       child: CheckboxListTile(
@@ -235,87 +175,3 @@ class _TodoMainState extends State<TodoMain> {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
